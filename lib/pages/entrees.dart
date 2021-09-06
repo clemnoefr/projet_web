7406e07273f2af0509e7f1be315046f5cb78772a
clemnoefr <57705783+clemnoefr@users.noreply.github.com>
2021-09-06 22:03:39 +0200

    build application
    modification pour pouvoir utiliser application sur émulateur android en remplacant la dépendance dart:html par dart:io

--- lib/pages/entrees.dart
+++ lib/pages/entrees.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:html';
+
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
--- lib/pages/repas.dart
+++ lib/pages/repas.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:html';
+
 import 'dart:typed_data';
 
 import 'package:flutter_posts/domain/post.dart';
